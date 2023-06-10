4ff57499772ced2bdd9771ade794c748d4fb01a4
Millard Harris <millard@freemountaininc.com>
2023-06-10 16:38:12 -0700

    Token Deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,11 +25,7 @@ module.exports = {
   networks: {
     localhost: {},
     goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: privateKeys.split(','),
-    },
-    mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: privateKeys.split(','),
     }
   },
--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -2,20 +2,18 @@ async function main() {
   console.log(`Preparing deployment...\n`)
 
   // Fetch contract to deploy
-  const Token = await ethers.getContractFactory("Token")
-
+  const Token = await ethers.getContractFactory('Token')
+ 
   // Deploy contracts
-  const token = await Token.deploy()
-  await token.deployed()
-  console.log(`Token Deployed to: ${token.address}`)
+  const dapp = await Token.deploy('Dapp University', 'DAPP', '1000000')
+  await dapp.deployed()
+  console.log(`DAPP Deployed to: ${dapp.address}`)
+  
+}
 
-  }
-
-
-  main()
+main()
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
     process.exit(1);
   });
-
